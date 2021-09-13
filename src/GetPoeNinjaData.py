f040f7c0a5193b1dd2964cd2ab8530164307e30e
leftember <leftember@gmail.com>
2021-09-13 05:38:03 +0000

    GetDataFromPoeNinja

---
+++ src/GetPoeNinjaData.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+league = 'Expedition'
+
+def GetData(category:str, type:str) -> None:
+    url = f'https://poe.ninja/api/data/{category}Overview?league={league}&type={type}&language=en'
+    response = requests.get(url)
+    cacheFile = f'../target/{type}.json'
+
+    with open(cacheFile, mode='w', encoding='utf-8') as fp:
+        json.dump(response.json(), fp)
+
+currencyTypes = ['Currency', 'Fragment']
+itemTypes = ['DivinationCard','Prophecy','Oil','Incubator','UniqueWeapon','UniqueArmour']
+itemTypes.extend(['UniqueAccessory','UniqueFlask','UniqueJewel','ClusterJewel','Fossil','Essence'])
+
+for t in currencyTypes:
+    GetData('Currency', t)
+
+for t in itemTypes:
+    GetData('Item', t)
+
